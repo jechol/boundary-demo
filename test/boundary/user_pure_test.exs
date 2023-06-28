b14f0492d5b137e247342a036640ad658332001c
Jechol Lee <mr.jechol@gmail.com>
2023-06-28 15:46:04 +0900

    done

--- test/boundary/user_pure_test.exs
+++ test/boundary/user_pure_test.exs
@@ -4,35 +4,21 @@ defmodule Boundary.User.PureTest do
   alias Boundary.User
   alias Boundary.User.Pure
 
-  describe "should_send_billing_reminder?" do
-    test "when user is active and paid_at is less than 30 days ago" do
-      refute Pure.should_send_billing_reminder?(
-               %User{
-                 active: true,
-                 paid_at: ~U[2021-01-01T00:00:00+00]
-               },
-               ~U[2021-01-20T00:00:00+00]
-             )
-    end
+  test "should_send_billing_reminder?" do
+    user = %User{
+      active: true,
+      paid_at: ~U[2021-01-01T00:00:00+00]
+    }
 
-    test "when user is active and paid_at is more than 30 days ago" do
-      assert Pure.should_send_billing_reminder?(
-               %User{
-                 active: true,
-                 paid_at: ~U[2021-01-01T00:00:00+00]
-               },
-               ~U[2021-02-01T00:00:00+00]
-             )
-    end
+    now = ~U[2021-02-10T00:00:00+00]
 
-    test "when paid_at is more than 30 days ago but user is  inactive" do
-      refute Pure.should_send_billing_reminder?(
-               %User{
-                 active: false,
-                 paid_at: ~U[2021-01-01T00:00:00+00]
-               },
-               ~U[2021-02-01T00:00:00+00]
-             )
-    end
+    assert Pure.should_send_billing_reminder?(user, now)
+
+    refute Pure.should_send_billing_reminder?(%User{user | active: false}, now)
+
+    refute Pure.should_send_billing_reminder?(
+             %User{user | paid_at: ~U[2021-01-10T00:00:00+00]},
+             now
+           )
   end
 end
